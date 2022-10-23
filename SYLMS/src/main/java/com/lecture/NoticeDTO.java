587c88c3db3c4e68901744f9aa55256425dd0964
sel2 <sel2@DESKTOP-CDQ96HS>
2022-10-24 03:23:53 +0900

    공지관련 추가

---
+++ SYLMS/src/main/java/com/lecture/NoticeDTO.java
@@ -0,0 +1,89 @@
+package com.lecture;
+
+public class NoticeDTO {
+	private String articleNo;
+	private String subjectNo;
+	private String bbsCode;
+	private String userId;
+	private String title;
+	private String content;
+	private String reg_date;
+	private int hitCount;
+	private String savefileName;
+	private String originalfileName;
+	private long fileSize;
+	
+	
+	public String getArticleNo() {
+		return articleNo;
+	}
+	public void setArticleNo(String articleNo) {
+		this.articleNo = articleNo;
+	}
+	public String getSubjectNo() {
+		return subjectNo;
+	}
+	public void setSubjectNo(String subjectNo) {
+		this.subjectNo = subjectNo;
+	}
+	public String getBbsCode() {
+		return bbsCode;
+	}
+	public void setBbsCode(String bbsCode) {
+		this.bbsCode = bbsCode;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getHitCount() {
+		return hitCount;
+	}
+	public void setHitCount(int hitCount) {
+		this.hitCount = hitCount;
+	}
+	public String getSavefileName() {
+		return savefileName;
+	}
+	public void setSavefileName(String savefileName) {
+		this.savefileName = savefileName;
+	}
+	public String getOriginalfileName() {
+		return originalfileName;
+	}
+	public void setOriginalfileName(String originalfileName) {
+		this.originalfileName = originalfileName;
+	}
+	public long getFileSize() {
+		return fileSize;
+	}
+	public void setFileSize(long fileSize) {
+		this.fileSize = fileSize;
+	}
+	
+	
+	
+	
+	
+	
+}
