5a7456150d1e1cb54f4d69c1a2aca530f9b15e75
ssj04 <ssj04@DESKTOP-M70FNUN>
2022-11-03 10:17:01 +0900

    일ㄴ단 시험분리

--- SYLMS/src/main/java/com/lecture/LectureDTO.java
+++ SYLMS/src/main/java/com/lecture/LectureDTO.java
@@ -27,51 +27,7 @@ private String attendNo;
 private String attend_pass;
 private String gen_time;
 
-private String examCode;
-private int score;
-private String examType;
-private String s_time;
-private String e_time;
-private String gradeCode;
 
-
-
-public String getExamCode() {
-	return examCode;
-}
-public void setExamCode(String examCode) {
-	this.examCode = examCode;
-}
-public int getScore() {
-	return score;
-}
-public void setScore(int score) {
-	this.score = score;
-}
-public String getExamType() {
-	return examType;
-}
-public void setExamType(String examType) {
-	this.examType = examType;
-}
-public String getS_time() {
-	return s_time;
-}
-public void setS_time(String s_time) {
-	this.s_time = s_time;
-}
-public String getE_time() {
-	return e_time;
-}
-public void setE_time(String e_time) {
-	this.e_time = e_time;
-}
-public String getGradeCode() {
-	return gradeCode;
-}
-public void setGradeCode(String gradeCode) {
-	this.gradeCode = gradeCode;
-}
 public String getAttendNo() {
 	return attendNo;
 }
