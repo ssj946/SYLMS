743b8ff04539ff1beb77b8a279017d1692991d4c
sel2 <sel2@DESKTOP-CDQ96HS>
2022-11-06 04:27:11 +0900

    학사공지 servlet이름 haksa임

---
+++ SYLMS/src/main/java/com/haksa/NoticeDAO.java
@@ -0,0 +1,796 @@
+package com.haksa;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.util.DBConn;
+
+public class NoticeDAO {
+	private Connection conn = DBConn.getConnection();
+
+	public void insertNotice(NoticeDTO dto) throws SQLException {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+		long seq;
+
+		try {
+			sql = "SELECT notice_seq.NEXTVAL FROM dual";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			seq = 0;
+			if (rs.next()) {
+				seq = rs.getLong(1);
+			}
+			dto.setNum(seq);
+
+			rs.close();
+			pstmt.close();
+			rs = null;
+			pstmt = null;
+
+			sql = "INSERT INTO notice(num, notice, userId, subject, content, hitCount, reg_date) "
+					+ "  VALUES (?, ?, ?, ?, ?, 0, SYSDATE)";
+
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, dto.getNum());
+			pstmt.setInt(2, dto.getNotice());
+			pstmt.setString(3, dto.getUserId());
+			pstmt.setString(4, dto.getSubject());
+			pstmt.setString(5, dto.getContent());
+
+			pstmt.executeUpdate();
+			
+			pstmt.close();
+			pstmt = null;
+
+			if (dto.getSaveFiles() != null) {
+				sql = "INSERT INTO noticeFile(fileNum, num, saveFilename, originalFilename) VALUES (noticeFile_seq.NEXTVAL, ?, ?, ?)";
+				pstmt = conn.prepareStatement(sql);
+				
+				for (int i = 0; i < dto.getSaveFiles().length; i++) {
+					pstmt.setLong(1, dto.getNum());
+					pstmt.setString(2, dto.getSaveFiles()[i]);
+					pstmt.setString(3, dto.getOriginalFiles()[i]);
+					
+					pstmt.executeUpdate();
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+	}
+
+	public int dataCount() {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT NVL(COUNT(*), 0) FROM notice";
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				result = rs.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return result;
+	}
+
+	// 검색에서 전체의 개수
+	public int dataCount(String condition, String keyword) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT NVL(COUNT(*), 0) FROM notice n "
+					+ " JOIN member1 m ON n.userId=m.userId ";
+			if (condition.equals("all")) {
+				sql += "  WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ";
+			} else if (condition.equals("reg_date")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sql += "  WHERE TO_CHAR(reg_date, 'YYYYMMDD') = ? ";
+			} else {
+				sql += "  WHERE INSTR(" + condition + ", ?) >= 1 ";
+			}
+
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, keyword);
+			if (condition.equals("all")) {
+				pstmt.setString(2, keyword);
+			}
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				result = rs.getInt(1);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return result;
+	}
+
+	// 게시물 리스트
+	public List<NoticeDTO> listNotice(int offset, int size) {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append(" SELECT num, n.userId, userName, subject, ");
+			sb.append("       hitCount, reg_date ");
+			sb.append(" FROM notice n ");
+			sb.append(" JOIN member1 m ON n.userId = m.userId ");
+			sb.append(" ORDER BY num DESC ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ");
+
+			pstmt = conn.prepareStatement(sb.toString());
+			
+			pstmt.setInt(1, offset);
+			pstmt.setInt(2, size);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNum(rs.getLong("num"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setReg_date(rs.getString("reg_date"));
+
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	// 검색에서 리스트
+	public List<NoticeDTO> listNotice(int offset, int size, String condition, String keyword) {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append(" SELECT num, n.userId, userName, subject, ");
+			sb.append("       hitCount, reg_date ");
+			sb.append(" FROM notice n ");
+			sb.append(" JOIN member1 m ON n.userId = m.userId ");
+			if (condition.equals("all")) {
+				sb.append(" WHERE INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ");
+			} else if (condition.equals("reg_date")) {
+				keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+				sb.append(" WHERE TO_CHAR(reg_date, 'YYYYMMDD') = ?");
+			} else {
+				sb.append(" WHERE INSTR(" + condition + ", ?) >= 1 ");
+			}
+			sb.append(" ORDER BY num DESC ");
+			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ");
+			
+			pstmt = conn.prepareStatement(sb.toString());
+			
+			if (condition.equals("all")) {
+				pstmt.setString(1, keyword);
+				pstmt.setString(2, keyword);
+				pstmt.setInt(3, offset);
+				pstmt.setInt(4, size);
+			} else {
+				pstmt.setString(1, keyword);
+				pstmt.setInt(2, offset);
+				pstmt.setInt(3, size);
+			}
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNum(rs.getLong("num"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setReg_date(rs.getString("reg_date")); // yyyy-MM-dd HH:mm:ss
+
+				list.add(dto);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	// 공지글
+	public List<NoticeDTO> listNotice() {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			sb.append(" SELECT num, n.userId, userName, subject, ");
+			sb.append("       hitCount, TO_CHAR(reg_date, 'YYYY-MM-DD') reg_date  ");
+			sb.append(" FROM notice n ");
+			sb.append(" JOIN member1 m ON n.userId=m.userId ");
+			sb.append(" WHERE notice=1  ");
+			sb.append(" ORDER BY num DESC ");
+
+			pstmt = conn.prepareStatement(sb.toString());
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setNum(rs.getLong("num"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setReg_date(rs.getString("reg_date"));
+
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	public NoticeDTO readNotice(long num) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT num, notice, n.userId, userName, subject, content, hitCount, reg_date "
+					+ " FROM notice n "
+					+ " JOIN member1 m ON n.userId=m.userId "
+					+ " WHERE num = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, num);
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+
+				dto.setNum(rs.getLong("num"));
+				dto.setNotice(rs.getInt("notice"));
+				dto.setUserId(rs.getString("userId"));
+				dto.setUserName(rs.getString("userName"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setHitCount(rs.getInt("hitCount"));
+				dto.setReg_date(rs.getString("reg_date"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+	}
+
+	// 이전글
+	public NoticeDTO preReadNotice(long num, String condition, String keyword) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			if (keyword != null && keyword.length() != 0) {
+				sb.append(" SELECT num, subject ");
+				sb.append(" FROM notice n ");
+				sb.append(" JOIN member1 m ON n.userId = m.userId ");
+				sb.append(" WHERE ( num > ? ) ");
+				if (condition.equals("all")) {
+					sb.append("   AND ( INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ) ");
+				} else if (condition.equals("reg_date")) {
+					keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+					sb.append("   AND ( TO_CHAR(reg_date, 'YYYYMMDD') = ? ) ");
+				} else {
+					sb.append("   AND ( INSTR(" + condition + ", ?) >= 1 ) ");
+				}
+				sb.append(" ORDER BY num ASC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				
+				pstmt.setLong(1, num);
+				pstmt.setString(2, keyword);
+				if (condition.equals("all")) {
+					pstmt.setString(3, keyword);
+				}
+			} else {
+				sb.append(" SELECT num, subject FROM notice ");
+				sb.append(" WHERE num > ? ");
+				sb.append(" ORDER BY num ASC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				
+				pstmt.setLong(1, num);
+			}
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+				
+				dto.setNum(rs.getLong("num"));
+				dto.setSubject(rs.getString("subject"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+	}
+
+	// 다음글
+	public NoticeDTO nextReadNotice(long num, String condition, String keyword) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			if (keyword != null && keyword.length() != 0) {
+				sb.append(" SELECT num, subject ");
+				sb.append(" FROM notice n ");
+				sb.append(" JOIN member1 m ON n.userId = m.userId ");
+				sb.append(" WHERE ( num < ? ) ");
+				if (condition.equals("all")) {
+					sb.append("   AND ( INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ) ");
+				} else if (condition.equals("reg_date")) {
+					keyword = keyword.replaceAll("(\\-|\\/|\\.)", "");
+					sb.append("   AND ( TO_CHAR(reg_date, 'YYYYMMDD') = ? ) ");
+				} else {
+					sb.append("   AND ( INSTR(" + condition + ", ?) >= 1 ) ");
+				}
+				sb.append(" ORDER BY num DESC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				
+				pstmt.setLong(1, num);
+				pstmt.setString(2, keyword);
+				if (condition.equals("all")) {
+					pstmt.setString(3, keyword);
+				}
+			} else {
+				sb.append(" SELECT num, subject FROM notice ");
+				sb.append(" WHERE num < ? ");
+				sb.append(" ORDER BY num DESC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				
+				pstmt.setLong(1, num);
+			}
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+				
+				dto.setNum(rs.getLong("num"));
+				dto.setSubject(rs.getString("subject"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+	}
+
+	public void updateHitCount(long num) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "UPDATE notice SET hitCount=hitCount+1 WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, num);
+			
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+	}
+
+	public void updateNotice(NoticeDTO dto) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "UPDATE notice SET notice=?, subject=?, content=? "
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, dto.getNotice());
+			pstmt.setString(2, dto.getSubject());
+			pstmt.setString(3, dto.getContent());
+			pstmt.setLong(4, dto.getNum());
+			
+			pstmt.executeUpdate();
+			
+			pstmt.close();
+			pstmt = null;
+
+			if (dto.getSaveFiles() != null) {
+				sql = "INSERT INTO noticeFile(fileNum, num, saveFilename, originalFilename) VALUES (noticeFile_seq.NEXTVAL, ?, ?, ?)";
+				pstmt = conn.prepareStatement(sql);
+				
+				for (int i = 0; i < dto.getSaveFiles().length; i++) {
+					pstmt.setLong(1, dto.getNum());
+					pstmt.setString(2, dto.getSaveFiles()[i]);
+					pstmt.setString(3, dto.getOriginalFiles()[i]);
+					
+					pstmt.executeUpdate();
+				}
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+	}
+
+	public void deleteNotice(long num) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "DELETE FROM notice WHERE num = ? ";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, num);
+			
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+	}
+
+	public void deleteNoticeList(long[] nums) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "DELETE FROM notice WHERE num IN (";
+			for (int i = 0; i < nums.length; i++) {
+				sql += "?,";
+			}
+			sql = sql.substring(0, sql.length() - 1) + ")";
+
+			pstmt = conn.prepareStatement(sql);
+			
+			for (int i = 0; i < nums.length; i++) {
+				pstmt.setLong(i + 1, nums[i]);
+			}
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e2) {
+				}
+			}
+		}
+
+	}
+
+	public List<NoticeDTO> listNoticeFile(long num) {
+		List<NoticeDTO> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT fileNum, num, saveFilename, originalFilename FROM noticeFile WHERE num = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, num);
+			
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				NoticeDTO dto = new NoticeDTO();
+
+				dto.setFileNum(rs.getLong("fileNum"));
+				dto.setNum(rs.getLong("num"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setOriginalFilename(rs.getString("originalFilename"));
+				
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return list;
+	}
+
+	public NoticeDTO readNoticeFile(long fileNum) {
+		NoticeDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT fileNum, num, saveFilename, originalFilename FROM noticeFile WHERE fileNum = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, fileNum);
+			
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new NoticeDTO();
+
+				dto.setFileNum(rs.getLong("fileNum"));
+				dto.setNum(rs.getLong("num"));
+				dto.setSaveFilename(rs.getString("saveFilename"));
+				dto.setOriginalFilename(rs.getString("originalFilename"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+
+		return dto;
+	}
+
+	public void deleteNoticeFile(String mode, long num) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			if (mode.equals("all")) {
+				sql = "DELETE FROM noticeFile WHERE num = ?";
+			} else {
+				sql = "DELETE FROM noticeFile WHERE fileNum = ?";
+			}
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setLong(1, num);
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e2) {
+				}
+			}
+		}
+
+	}
+
+}
---
+++ SYLMS/src/main/webapp/WEB-INF/views/haksa/noticeWrite.jsp
@@ -0,0 +1,172 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>SYLMS</title>
+<jsp:include page="/WEB-INF/views/layout/staticHeader.jsp"/>
+<style type="text/css">
+
+ul{
+
+list-style: none;
+}
+</style>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/board2.css" type="text/css">
+
+<script type="text/javascript">
+function sendOk() {
+    const f = document.boardForm;
+	let str;
+	
+    str = f.title.value.trim();
+    if(!str) {
+        alert("제목을 입력하세요. ");
+        f.subject.focus();
+        return;
+    }
+
+    str = f.content.value.trim();
+    if(!str) {
+        alert("내용을 입력하세요. ");
+        f.content.focus();
+        return;
+    }
+
+	f.action = "${pageContext.request.contextPath}/notice/notice${mode}_ok.do"; 
+    f.submit();
+}
+
+
+<c:if test="${mode=='update'}">
+function deleteFile(fileNo) {
+	if(! confirm("파일을 삭제 하시겠습니까 ?")) {
+		return;
+	}
+	let url = "${pageContext.request.contextPath}/notice/deleteFile.do?articleNo=" + ${dto.articleNo}+"&subjectNo=${subjectNo}&fileNo="+fileNo;
+	location.href = url;
+}
+</c:if>
+</script>
+
+</head>
+
+<body>
+
+<header>
+	<jsp:include page="/WEB-INF/views/layout/header.jsp"/>
+</header>
+	
+<main>
+<section>
+	<div class="container-fluid">
+		<div class="row" style="line-height: 1.5rem">&nbsp;</div>
+		<div class="row">
+			<div class="col-lg-1 bg-dark bg-gradient" >
+			<!-- brief 사이드바 자리 -->
+			<jsp:include page="/WEB-INF/views/layout/brief_sidebar.jsp"/>
+			</div>
+			<div class="col-lg-11 ms-auto">
+			
+			<!-- classroom header 자리 -->
+			<div class="row">
+			<jsp:include page="/WEB-INF/views/layout/classroom_header.jsp"/>
+			</div>
+			<div class="row">
+			<!-- 강의 사이드바 자리 -->
+			<div class="col-xl-2 col-md-2 col-lg-2 bg-black bg-gradient" style="box-shadow: none;">
+			<jsp:include page="/WEB-INF/views/layout/lecture_sidebar.jsp"/>
+			</div>
+			
+			<!-- 본문 -->
+			<div class="col-lg-10 gap-3 ms-auto">
+				<div class="ms-5 me-5 pt-3 mt-4 mb-5">
+		
+			<div class="body-title">
+				<h3><i class="fas fa-microphone fa-1x"></i> 공지사항 작성 </h3>
+			</div>
+			
+			<div class="body-main">
+				<form name="boardForm" method="post"  enctype="multipart/form-data">
+					<table class="table write-form mt-5">
+						<tr>
+							<td class="table-light col-sm-2" scope="row">제 목</td>
+							<td>
+								<input type="text" name="title" maxlength="100" class="form-control" value="${dto.title}">
+							</td>
+						</tr>
+	        
+						<tr>
+							<td class="table-light col-sm-2" scope="row">작성자명</td>
+	 						<td>
+								<p class="form-control-plaintext">${sessionScope.member.userName}</p>
+							</td>
+						</tr>
+	
+						<tr>
+							<td class="table-light col-sm-2" scope="row">내 용</td>
+							<td>
+								<textarea name="content" id="content" class="form-control">${dto.content}</textarea>
+							</td>
+						</tr>
+						
+						<tr>
+							<td class="table-light col-sm-2">첨&nbsp;&nbsp;&nbsp;&nbsp;부</td>
+							<td> 
+								<input type="file" name="selectFile" multiple="multiple" class="form-control">
+							</td>
+						</tr>
+						<c:if test="${mode=='update'}">
+							<c:forEach var="vo" items="${listFile}">
+								<tr>
+									<td class="table-light col-sm-2" scope="row">첨부된파일</td>
+									<td> 
+										<p class="form-control-plaintext">
+											<a href="javascript:deleteFile('${vo.fileNo}');"><i class="bi bi-trash"></i></a>
+											${vo.originalFilename}
+										</p>
+									</td>
+								</tr>
+							</c:forEach> 
+						</c:if>
+					
+					</table>
+					
+					<table class="table table-borderless">
+	 					<tr>
+							<td class="text-center">
+								<button type="button" class="btn btn-dark" onclick="sendOk();">${mode=='update'?'수정완료':'등록하기'}&nbsp;<i class="bi bi-check2"></i></button>
+								<button type="reset" class="btn btn-light">다시입력</button>
+								<button type="button" class="btn btn-light" onclick="location.href='${pageContext.request.contextPath}/notice/notice.do?subjectNo=${subjectNo}';">${mode=='update'?'수정취소':'등록취소'}&nbsp;<i class="bi bi-x"></i></button>
+								<input type="hidden" name="subjectNo" value="${subjectNo}">
+								<c:if test="${mode=='update'}">
+									<input type="hidden" name="articleNo" value="${dto.articleNo}">
+									<input type="hidden" name="subjectNo" value="${dto.subjectNo}">
+									<input type="hidden" name="page" value="${page}">
+								</c:if>
+							</td>
+						</tr>
+					</table>
+				</form>
+			</div>
+
+					
+				</div>
+			<!-- 본문 끝 -->
+			</div>
+			</div>
+
+				
+				</div>
+			</div>
+		</div>
+	
+	</section>
+</main>
+<jsp:include page="/WEB-INF/views/layout/staticFooter.jsp"/>
+</body>
+</html>
